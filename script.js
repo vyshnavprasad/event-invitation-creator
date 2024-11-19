b9cae568de95a0d3247321325a175926bdb54660
Vyshnav Prasad <vyshnavprasad.va@gmail.com>
2024-11-19 12:32:37 +0530

    #3 Invitation card genaration - Modified

--- script.js
+++ script.js
@@ -6,26 +6,25 @@ const formValidationDialogClose = document.getElementById("form-validation-dialo
 invitationFormSubmit.addEventListener("click", (e) => {
     e.preventDefault();
     // Get Form-data
-    
     const formData = new FormData(invitationForm);
-    const formDataObject = Object.fromEntries(formData.entries());
     
     // Form Validation and Submition
     if (Array.from(formData.entries()).reduce((a, c) => 
-            a & (c[1].length>0), true)) {
+            a & (c[1].length>0) , true)) {
 
-        document.querySelector('#card-event-name').textContent = formDataObject['event-name'];
-        document.querySelector('#card-location').textContent = formDataObject['location'];
-        document.querySelector('#card-day-date').textContent = "";
+        document.querySelector('#card-event-name').textContent = formData.get('event-name');
+        document.querySelector('#card-location').textContent = formData.get('location');
+        document.querySelector('#card-day-date').textContent = 
+            new Date(formData.get('event-date')).toDateString();
         document.querySelector('#card-time').textContent = 
-            formDataObject['start-time'] + " + " + formDataObject['end-time'];
-        document.querySelector('#card-location').textContent = formDataObject['location'];
-        document.querySelector('#card-description').textContent = formDataObject['event-description'];
+            formData.get('start-time') + " + " + formData.get('end-time');
+        document.querySelector('#card-location').textContent = formData.get('location');
+        document.querySelector('#card-description').textContent = formData.get('event-description');
 
         document.querySelector('#input-form').style.display = "none";
         document.querySelector('#invitation-card').style.display = "block";
 
-        document.querySelector('#body').style.background = "white";
+        document.querySelector('#body').style.background = "lightgray";
     } else formValidationDialog.showModal();
 })
     
