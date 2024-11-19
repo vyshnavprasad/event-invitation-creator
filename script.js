5e2b81593c9d214d4bb1a97d2e3195a825abc89d
Vyshnav Prasad <vyshnavprasad.va@gmail.com>
2024-11-19 11:31:28 +0530

    #3 Invitation card genaration - Created

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ const formValidationDialogClose = document.getElementById("form-validation-dialo
 
 invitationFormSubmit.addEventListener("click", (e) => {
     e.preventDefault();
+    // Get Form-data
     
-    // Form Validation
-    const formData = new FormData(invitationForm)
+    const formData = new FormData(invitationForm);
+    const formDataObject = Object.fromEntries(formData.entries());
+    
+    // Form Validation and Submition
     if (Array.from(formData.entries()).reduce((a, c) => 
-        a & (c[1].length>0)
-    , true)) {
+            a & (c[1].length>0), true)) {
+
+        document.querySelector('#card-event-name').textContent = formDataObject['event-name'];
+        document.querySelector('#card-location').textContent = formDataObject['location'];
+        document.querySelector('#card-day-date').textContent = "";
+        document.querySelector('#card-time').textContent = 
+            formDataObject['start-time'] + " + " + formDataObject['end-time'];
+        document.querySelector('#card-location').textContent = formDataObject['location'];
+        document.querySelector('#card-description').textContent = formDataObject['event-description'];
+
+        document.querySelector('#input-form').style.display = "none";
+        document.querySelector('#invitation-card').style.display = "block";
+
+        document.querySelector('#body').style.background = "white";
     } else formValidationDialog.showModal();
 })
     
